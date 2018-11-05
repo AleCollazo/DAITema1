e5825ee2fd3964e10ac050a08d886ad2aacfd80e
Alejandro <alecollazofdez@gmail.com>
2018-11-05 13:56:40 +0100

    Ejercicio 6 de servivios empezado

---
+++ Tema1Serv/Ejercicio6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static readonly object l = new object();
+
+        static void Main(string[] args)
+        {
+            Random ram = new Random();
+        }
+
+        static void player(Random ram, int leftPosition)
+        {
+            int num;
+
+            while (true) {
+                lock (l) {
+                    num = ram.Next(1, 10);
+                    Console.WriteLine("{0}", 100 * num);
+                    Console.SetCursorPosition(leftPosition, 5);
+                }
+            }
+        }
+
+    }
+}
