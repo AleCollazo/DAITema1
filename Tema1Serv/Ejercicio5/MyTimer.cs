7c8a9939412cfbc63f2f7e4ffd8b1f3bd25b6f9a
Alejandro <alecollazofdez@gmail.com>
2018-11-05 13:10:47 +0100

    Ejercicio 5 de servicios

---
+++ Tema1Serv/Ejercicio5/MyTimer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    public delegate void delegateFunction();
+    class MyTimer
+    {
+        public int Interval { set; get; }
+        private Thread thread;
+        private delegateFunction function;
+        static readonly object l = new object();
+        private bool toStop = true;
+
+        public MyTimer(delegateFunction function)
+        {
+            this.function = function;
+            thread = new Thread(timer);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void timer()
+        {
+            while (true)
+            {
+                lock (l) {
+                    if (toStop)
+                    {
+                        Monitor.Wait(l);
+                    }
+                        function.Invoke();
+                }
+                Thread.Sleep(Interval);
+            }
+        }
+
+        public void star()
+        {
+            toStop = false;
+            lock (l)
+            {
+                Monitor.Pulse(l);
+            }
+        }
+
+        public void stop()
+        {
+
+            toStop = true;
+        }
+
+    }
+}
---
+++ Tema1Serv/Ejercicio5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+
+    class Program
+    {
+        static int counter = 0;
+        static readonly object l = new object();
+
+        static void increment()
+        {
+            counter++;
+            Console.SetCursorPosition(5, 10);
+            Console.WriteLine(counter);
+        }
+
+        private static void pintarCounter()
+        {
+            Console.SetCursorPosition(5, 10);
+            Console.WriteLine(counter);
+            Console.SetCursorPosition(0, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            MyTimer t = new MyTimer(increment);
+            t.Interval = 1000;
+            string op = "";
+
+            do
+            {
+                Console.WriteLine("Press any key to start");
+                Console.ReadKey();
+                t.star();
+                Console.Clear();
+                Console.WriteLine("Press any key to stop");
+                Console.ReadKey();
+                t.stop();
+                Console.Clear();
+                pintarCounter();
+                Console.WriteLine("Press 1 to restart or Enter to end");
+                op = Console.ReadLine();
+                Console.Clear();
+                pintarCounter();
+            } while (op == "1");
+        }
+    }
+}
